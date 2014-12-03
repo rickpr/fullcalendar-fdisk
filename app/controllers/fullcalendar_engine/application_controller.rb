5f86147028564c41e8fe1c47afcbc61a634affa4
Ricardo Piro-Rael <fdisk@fdisk.co>
2014-12-03 14:47:29 -0700

    Checking if I can access model

--- app/controllers/fullcalendar_engine/application_controller.rb
+++ app/controllers/fullcalendar_engine/application_controller.rb
@@ -1,4 +1,10 @@
 module FullcalendarEngine
   class ApplicationController < ActionController::Base
+    #helper_method :current_user
+
+    #def current_user
+    #  User.find_by_netid(session[:cas_user]) if User.find_by_netid(session[:cas_user])
+    #end
+
   end
 end
